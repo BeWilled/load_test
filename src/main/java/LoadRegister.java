caf6d9f8885ff7fc3d332dab41b2014c7277dbe6
mbehlok <mbehloks@caracoltv.com.co>
2016-08-24 16:33:15 -0500

    first commit

--- src/main/java/LoadRegister.java
+++ src/main/java/LoadRegister.java
@@ -11,7 +11,7 @@
 public class LoadRegister {
     
     public static void main(String a[]){
-                int threads = 1;
+                int threads = Integer.valueOf(a[0]);
 
         for(int i = 0 ; i < threads; i++){
             LoadRegisterThread ltt = new LoadRegisterThread(i);
--- src/main/java/LoadRegisterThread.java
+++ src/main/java/LoadRegisterThread.java
@@ -23,16 +23,17 @@ public class LoadRegisterThread extends Thread {
         while(true){
 try{
     
-               Connection.Response res = Jsoup.connect("https://www.caracolplay.com/suscripcion/login?destination=suscripcion").followRedirects(true).timeout(0).header("Loadtest","X1256836811").header("Host","www.caracolplay.com").userAgent("Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36").execute();
+               Connection.Response res = Jsoup.connect("http://www.caracolplay.com/suscripcion/login?destination=suscripcion").followRedirects(true).timeout(0).header("Loadtest","X1256836811").header("Host","www.caracolplay.com").userAgent("Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36").execute();
                String url = res.url().toString();
                // System.out.println(url);
                Document res2 = res.parse();
                
                String fid = res2.getElementsByAttributeValueMatching("name", "form_build_id").get(1).attr("value");
-               System.out.println(fid);
-               Document doc = Jsoup.connect(url).followRedirects(true).header("Referer", "https://www.caracolplay.com/suscripcion/login?destination=suscripcion").cookies(res.cookies()).timeout(0).header("Loadtest","X1256836811").header("Host","www.caracolplay.com").userAgent("Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36").
+               //System.out.println(fid);
+               Document doc = Jsoup.connect(url).followRedirects(true).header("Referer", "http://www.caracolplay.com/suscripcion/login?destination=suscripcion").cookies(res.cookies()).timeout(0).header("Loadtest","X1256836811").header("Host","www.caracolplay.com").userAgent("Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36").
                data("name","mbehlok").data("pass","CApSNtc9").data("form_build_id",fid).data("form_id","user_login_block").data("op","Ingresar").post();
-               System.out.println(doc.html());
+               //System.out.println(doc.html());
+               System.out.println("Logged in from thread: "+id);
 }catch(Exception e){
     
     e.printStackTrace();
